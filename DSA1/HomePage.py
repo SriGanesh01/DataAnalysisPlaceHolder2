a8d1bb5228a7e4c50205f8f3a53af4ec5414a113
SriGanesh01 <psriganesh2005@gmail.com>
2023-11-11 16:06:40 +0530

    Set common theame

--- DSA1/HomePage.py
+++ DSA1/HomePage.py
@@ -14,22 +14,24 @@ def about():
 @app.route('/tabletop10')
 @app.route('/datastop10')
 def tabletop10():
+    route_name = 'Top 10 Values'
     tables = []
     for dataframe in Datas.CryptoData1_CC:
         top_10_rows = dataframe.head(10)
         html_table = top_10_rows.to_html(index=False)
         tables.append(html_table)
-    return render_template('datas.html', tables=tables, titles=['','binance-coin', 'bitcoin', 'bitcoin-cash', 'bitcoin-sv', 'cardano', 'eos', 'ethereum', 'litecoin', 'stellar', 'tether', 'tezos', 'xrp'])
+    return render_template('datas.html', tables=tables, route_name=route_name, titles=['', 'binance-coin', 'bitcoin', 'bitcoin-cash', 'bitcoin-sv', 'cardano', 'eos', 'ethereum', 'litecoin', 'stellar', 'tether', 'tezos', 'xrp'])
 
 @app.route('/tablebottom10')
 @app.route('/datasbottom10')
 def tablebottom10():
+    route_name = 'Last 10 Values'
     tables = []
     for dataframe in Datas.CryptoData1_CC:
         top_10_rows = dataframe.tail(10)
         html_table = top_10_rows.to_html(index=False)
         tables.append(html_table)
-    return render_template('datas.html', tables=tables, titles=['','binance-coin', 'bitcoin', 'bitcoin-cash', 'bitcoin-sv', 'cardano', 'eos', 'ethereum', 'litecoin', 'stellar', 'tether', 'tezos', 'xrp'])
+    return render_template('datas.html', tables=tables, route_name=route_name, titles=['', 'binance-coin', 'bitcoin', 'bitcoin-cash', 'bitcoin-sv', 'cardano', 'eos', 'ethereum', 'litecoin', 'stellar', 'tether', 'tezos', 'xrp'])
 
 @app.route('/analysis')
 def analysis():
